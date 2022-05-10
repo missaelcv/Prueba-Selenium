538e862daab87cfb423a65fd2b145548e4ee9a8f
missaelcv <missaelcv0220@gmail.com>
2022-05-09 21:33:18 -0400

    new variable

--- PruebaText.js
+++ PruebaText.js
@@ -1,2 +1,10 @@
+const { Builder, Key, until } = require("selenium-webdriver");
+
 const webdriver = requiere ('Seleniun webdriver');
 const assert = requiere("assert").strict;
+const { Builder, By, Key, until } = webdriver;
+const chrome = require ('selenium-webdriver/chrome');
+let options = new chrome.Options();
+let driver = new Builder().forBrowser('chrome').setChromeOption(options).Builder();
+
+
